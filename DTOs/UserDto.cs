177f0f4156f1c6eb53b37d5602f4b6293f651584
Basith P <basithmpy@outlook.com>
2021-11-01 09:28:16 +0530

    testing push with userdto

--- DTOs/UserDto.cs
+++ DTOs/UserDto.cs
@@ -7,5 +7,9 @@ namespace BookStoreBackEnd.DTOs
 {
     public class UserDto
     {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime LoggedOn { get; set; }
     }
 }
